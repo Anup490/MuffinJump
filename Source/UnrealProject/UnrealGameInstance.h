60ec6c09a6250e834fad0a35e1b3f5bad78ac144
Anup490 <anupbhandari490@gmail.com>
2020-09-02 08:52:11 +0545

    Added feature to make camera follow pawn

---
+++ Source/UnrealProject/UnrealGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "UnrealGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPROJECT_API UUnrealGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
